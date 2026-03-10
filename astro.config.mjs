540f815d0626356fea2a2660f8e1521ca611ab26
Karan Oberoi <koberoi1017@gmail.com>
2026-03-10 00:24:23 -0700

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,19 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://sprigandivy.com',
   integrations: [sitemap()],
+  redirects: {
+    '/blog.html': '/',
+    '/blog': '/',
+    '/portfolio.html': '/',
+    '/portfolio': '/',
+    '/decor.html': '/',
+    '/decor': '/',
+    '/arches.html': '/',
+    '/arches': '/',
+    '/terms.html': '/terms/',
+    '/contact.html': '/',
+    '/contact': '/',
+    '/what-are-day-of-coordinators.html': '/',
+    '/what-are-day-of-coordinators': '/',
+  },
 });
